b1a4f989fc780f559cf5f5444e1253cc4b5752c2
Joss <colombetjosselyn@gmail.com>
2021-03-25 12:51:17 +0100

    add get users route

--- routers/users.js
+++ routers/users.js
@@ -1,9 +1,10 @@
 var express = require('express')
 var router = express.Router()
+var userModel = require('../models/user')
 
-router.get('/', (request, response) => {
-    let { id } = request.query
-    response.json({ id })
+router.get('/', async (request, response) => {
+    var users = await userModel.find({}) // == "SELECT * from users"
+    response.json({ users })
 })
 
 router.post('/', (request, response) => {

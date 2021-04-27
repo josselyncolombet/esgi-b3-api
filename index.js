cbadcb9ac6cc5079eee1754f60f831336a77a308
Joss <colombetjosselyn@gmail.com>
2021-04-27 15:17:15 +0200

    remove mongodb

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var express = require('express')
 var routerUsers = require('./routers/users')
 var routerHotels = require('./routers/hotels')
-var mongoose = require('mongoose')
+/*var mongoose = require('mongoose')
 
 mongoose.Promise = Promise
 mongoose.connect('mongodb://localhost:27017/b3api', {useNewUrlParser: true, useUnifiedTopology: true})
 
 var db = mongoose.connection
 db.on('error', console.error.bind(console, "connection error: "))
-db.once('open', () => console.log('status :', db.states[db._readyState]))
+db.once('open', () => console.log('status :', db.states[db._readyState]))*/
 
 var app = express()
 
@@ -16,8 +16,8 @@ app.use(express.json())
 app.get('/', (req,res) => {
     res.json('Ok')
 })
-app.use('/hotels', routerHotels)
-app.use('/users', routerUsers)
+//app.use('/hotels', routerHotels)
+//app.use('/users', routerUsers)
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log('Server is running on', PORT)
